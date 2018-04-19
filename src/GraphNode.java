5b502b9bd1cb9c8e68ddc04e2c2b93d411c3e099
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2018-04-19 13:29:14 +0100

    Find all paths Depth First

--- src/GraphNode.java
+++ src/GraphNode.java
@@ -4,8 +4,10 @@ import java.util.List;
 public class GraphNode {
 
     private String name;
-    private List<GraphLink> graphLinkList=new ArrayList<>();
-
+    public AdjacencyMatrix mat;
+    public int nodeId;
+    public List<GraphLink> graphLinkList=new ArrayList<>();
+    public List<GraphNode> adjList=new ArrayList<>();
     public List<GraphLink> getGraphLinkList() {
         return graphLinkList;
     }
@@ -23,11 +25,45 @@ public class GraphNode {
     }
 
     public GraphNode(String name) {
-        this.setName(name);
+        this.name=name;
+        /*this.mat=mat;
+        mat.nodes[nodeId=mat.nodeCount++]=this;*/
+    }
+    public static List<List<GraphNode>> findAllPathsDepthFirst(GraphNode from, List<GraphNode> encountered, String lookingfor){
+        List<List<GraphNode>> result=null, temp2;
+        if(from.name.equals(lookingfor)) { //Found it
+            List<GraphNode> temp=new ArrayList<>(); //Create new single solution path list
+            temp.add(from); //Add current node to the new single path list
+            result=new ArrayList<>(); //Create new "list of lists" to store path permutations
+            result.add(temp); //Add the new single path list to the path permutations list
+            return result; //Return the path permutations list
+        }
+        if(encountered==null) encountered=new ArrayList<>(); //First node so create new (empty) encountered list
+        encountered.add(from); //Add current node to encountered list
+        for(GraphNode adjNode : from.adjList){
+            if(!encountered.contains(adjNode)) {
+                temp2=findAllPathsDepthFirst(adjNode,new ArrayList<>(encountered),lookingfor); //Use clone of encountered list
+//for recursive call!
+                if(temp2!=null) { //Result of the recursive call contains one or more paths to the solution node
+                    for(List<GraphNode> x : temp2) //For each partial path list returned
+                        x.add(0,from); //Add the current node to the front of each path list
+                    if(result==null) result=temp2; //If this is the first set of solution paths found use it as the result
+                    else result.addAll(temp2); //Otherwise append them to the previously found paths
+                }
+            }
+        }
+        return result;
+    }
+
+    public void connectToNodeDirected(GraphNode sourceNode, GraphNode destNode, int cost, char roadType) {
+        mat.amat[nodeId][destNode.nodeId]=true;
+        graphLinkList.add(new GraphLink(sourceNode, destNode, cost, roadType));
     }
 
     public void connectToNodeUndirected(GraphNode sourceNode, GraphNode destNode, int cost, char roadType) {
+        mat.amat[nodeId][destNode.nodeId]=mat.amat[destNode.nodeId][nodeId]=true;
         graphLinkList.add( new GraphLink(sourceNode, destNode,cost, roadType) ); //Add new link object to source adjacency list
         destNode.graphLinkList.add( new GraphLink(destNode, sourceNode, cost, roadType) ); //Add new link object to destination adjacency list
     }
+
 }
