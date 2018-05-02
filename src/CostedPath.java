9325ee081ff722900487ad6971d21a351cf9c22d
Dylan-Morrissey <dylanmorrissey2010@gmail.com>
2018-05-02 13:52:51 +0100

    Dijkstra Test

--- src/CostedPath.java
+++ src/CostedPath.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
 import java.util.List;
 
-//New class to hold a CostedPath object i.e. a list of GraphNode objects and a total cost attribute
 public class CostedPath {
 
-    private static List<CostedPath> allPaths = new ArrayList<>(); //Collection for all candidate costed paths from this node
+    private static List<CostedPath> allPaths = new ArrayList<>();
     public int pathCost = 0;
     private int pathTime = 0;
     public List<GraphNode> pathList = new ArrayList<>();
--- src/GraphNode.java
+++ src/GraphNode.java
@@ -65,38 +65,37 @@ public class GraphNode {
                     for(GraphNode n : encountered) {
                         for(GraphLink e : n.graphLinkList)
                             if(e.getDestNode()==currentNode && currentNode.nodeValue-e.getDistance()==n.nodeValue){
-//current node and the difference in node values is the cost of the edge -> found path node!
-                                cp.pathList.add(0, n); //Add the identified path node to the front of the result list
+
+                                cp.pathList.add(0, n);
 
                                 roadTypes.add(e.getRoadType().toString());
-                                currentNode=n; //Move the currentNode reference back to the identified path node
-                                foundPrevPathNode=true; //Set the flag to break the outer loop
-                                break; //We've found the correct previous path node and moved the currentNode reference
-//back to it so break the inner loop
+                                currentNode=n;
+                                foundPrevPathNode=true;
+                                break;
                             }
-                        if(foundPrevPathNode) break; //We've identified the previous path node, so break the inner loop to continue
+                        if(foundPrevPathNode) break;
                     }
                 }
-//Reset the node values for all nodes to (effectively) infinity so we can search again (leave no footprint!)
+
                 for(GraphNode n : encountered) n.nodeValue=Integer.MAX_VALUE;
                 for(GraphNode n : unencountered) n.nodeValue=Integer.MAX_VALUE;
                 System.out.println(cp.pathCost);
                 System.out.println(cp.pathList.toString());
-                return; //The costed (cheapest) path has been assembled, so return it!
+                return;
             }
-//We're not at the goal node yet, so...
-            for(GraphLink e : currentNode.graphLinkList) //For each edge/link from the current node...
-                if(!encountered.contains(e.getDestNode())) { //If the node it leads to has not yet been encountered (i.e. processed)
-                    e.getDestNode().nodeValue=Integer.min(e.getDestNode().nodeValue, currentNode.nodeValue+e.getDistance()); //Update the node value at the end
-                    //of the edge to the minimum of its current value or the total of the current node's value plus the cost of the edge
+
+            for(GraphLink e : currentNode.graphLinkList)
+                if(!encountered.contains(e.getDestNode())) {
+                    e.getDestNode().nodeValue=Integer.min(e.getDestNode().nodeValue, currentNode.nodeValue+e.getDistance());
+
                     unencountered.add(e.getDestNode());
                 }
-            unencountered.sort((n1, n2) -> n1.nodeValue - n2.nodeValue); //Sort in ascending node value order
+            unencountered.sort((n1, n2) -> n1.nodeValue - n2.nodeValue);
         }while(!unencountered.isEmpty());
     }
 
     public void connectToNodeUndirected(GraphNode sourceNode, GraphNode destNode, int cost, char roadType) {
-        graphLinkList.add(new GraphLink(sourceNode, destNode, cost, roadType)); //Add new link object to source adjacency list
+        graphLinkList.add(new GraphLink(sourceNode, destNode, cost, roadType));
         destNode.graphLinkList.add( new GraphLink(destNode, sourceNode, cost, roadType) ); //Add new link object to destination adjacency list
     }
 
